fad75963b72b353beb599bf1498dabd7cf102cb4
Joel Dueck <joel@jdueck.net>
2020-07-26 12:38:58 -0500

    Font updates

--- config.el
+++ config.el
@@ -15,7 +15,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "IBM Plex Mono" :size 17))
+(setq doom-font (font-spec :family "IBM Plex Mono" :size 15))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -81,7 +81,7 @@
  :i "M-_" "—"
  :i "M-;" "…")
 
-(defvar velcro-fonts '("IBM Plex Mono" "Triplicate T4c"))
+(defvar velcro-fonts '("Rec Mono SemiCasual" "IBM Plex Mono" "Triplicate T4c"))
 (defvar velcro-themes '(doom-one almost-mono-white))
 
 (defun make-cycler (lst func)
